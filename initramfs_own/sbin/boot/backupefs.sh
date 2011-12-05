f87882d4e1bb2a4d5c41c23056f45b50e1375e2a
Frank Meyering <frank@frank-meyering.de>
2011-12-05 22:57:51 +0100

    Modifed EFS Backup

--- initramfs_own/sbin/boot/backupefs.sh
+++ initramfs_own/sbin/boot/backupefs.sh
@@ -5,12 +5,11 @@ then
   mkdir /data/.FM
   chmod 777 /data/.FM
   /sbin/ext/busybox tar zcvf /data/.FM/backupefs.tar.gz /efs
-  /sbin/ext/busybox cat /dev/block/mmcblk0p1 > /data/.FM/efsdev-mmcblk0p1.img
-  /sbin/ext/busybox gzip /data/.FM/efsdev-mmcblk0p1.img
+  #/sbin/ext/busybox cat /dev/block/mmcblk0p1 > /data/.FM/efsdev-mmcblk0p1.img
+  #/sbin/ext/busybox gzip /data/.FM/efsdev-mmcblk0p1.img
   #make sure that sdcard is mounted, media scanned..etc
   (
     sleep 500
-    /sbin/ext/busybox cp /data/.FM/efs* /sdcard
+    /sbin/ext/busybox cp /data/.FM/backupefs.tar.gz /sdcard
   ) &
 fi
-
